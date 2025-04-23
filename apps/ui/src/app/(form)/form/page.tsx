abb7e7c1dcbecaea2a13a89b470dc20d396af3a0
Bruno Germano <bruno.germano@gmail.com>
2025-04-23 17:28:46 -0300

    update form page

--- apps/ui/src/app/(form)/form/page.tsx
+++ apps/ui/src/app/(form)/form/page.tsx
@@ -1,22 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import LeadForm from "@/components/lead-form";
 
 export default function FormPage() {
-    return (
-        <div className="w-full md:w-[45rem] mx-auto mt-10">
-            {/* TODO: make this form a client component */}
-            <form  className="space-y-4">
-                <div>
-                    <Label htmlFor="name">Name</Label>
-                    <Input id="name" name="name" type="text" placeholder="Enter your name" required />
-                </div>
-                <div>
-                    <Label htmlFor="email">Email</Label>
-                    <Input id="email" name="email" type="email" placeholder="Enter your email" required />
-                </div>
-                <Button type="submit">Submit</Button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-6 w-full md:w-[45rem] mx-auto mt-10">
+      <p className="text-sm text-muted-foreground">step 2</p>
+      <h2 className="text-xl font-bold">Tell us about yourself</h2>
+      <LeadForm />
+    </div>
+  );
 }
