75761d136dadf9859d94d0dfac02d98b020eba9f
Bruno Germano <bruno.germano@gmail.com>
2025-05-07 12:46:34 -0300

    update pages and folders structure

--- apps/ui/src/app/(auth)/signin/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/login-form";
-
-export default function SignIn() {
-  return (
-    <div className="grow h-full flex flex-col items-center justify-center">
-      <LoginForm />
-    </div>
-  );
-}
