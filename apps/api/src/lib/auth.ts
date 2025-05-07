51267d80fb48a55c03c8de7cca14b9fb7a0cd59b
Bruno Germano <bruno.germano@gmail.com>
2025-05-07 18:37:31 -0300

    move auth

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -1,12 +1,17 @@
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "@/constants";
+import { db } from "@/db/db";
+import * as schema from "@/db/schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "./constants";
-import { db } from "./db/db";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite", // or "mysql", "sqlite"
+    schema: {
+      ...schema,
+    }
   }),
+  trustedOrigins: ["http://localhost:3000"],
   socialProviders: {
     github: {
       clientId: GITHUB_CLIENT_ID,
