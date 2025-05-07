2c91608410e040c1182b0176a4c217de2d332b01
Bruno Germano <bruno.germano@gmail.com>
2025-05-07 19:55:43 -0300

    Update db connection

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -9,3 +9,6 @@ export const DB_FILE_NAME = process.env.DB_FILE_NAME!;
 
 export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID!;
 export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET!;
+
+export const DATABASE_URL = process.env.DATABASE_URL!;
+export const DATABASE_AUTH_TOKEN = process.env.DATABASE_AUTH_TOKEN!;
--- apps/api/src/db/db.ts
+++ apps/api/src/db/db.ts
@@ -1,10 +1,9 @@
-import { createClient } from "@libsql/client";
-import "dotenv/config";
+import { DATABASE_AUTH_TOKEN, DATABASE_URL } from "@/constants";
 import { drizzle } from "drizzle-orm/libsql";
 
-const client = createClient({
-  url: process.env.DATABASE_URL!,
-  authToken: process.env.DATABASE_AUTH_TOKEN!,
+export const db = drizzle({
+  connection: {
+    url: DATABASE_URL,
+    authToken: DATABASE_AUTH_TOKEN,
+  },
 });
-
-export const db = drizzle({ client });
