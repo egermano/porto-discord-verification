08c9f9da8ae63a491cdd6a6b5e60c912e96ba6b5
Bruno Germano <bruno.germano@gmail.com>
2025-05-07 18:37:55 -0300

    update auth API

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,21 @@
-import { auth } from "@/auth";
+import { auth } from "@/lib/auth";
+import { createRouter } from "@/lib/create-app";
 import { Hono } from "hono";
-const app = new Hono();
 
-app.on(["POST", "GET"], "/auth/**", (c) => auth.handler(c.req.raw));
+const app = new Hono();
+const authRouter = createRouter();
+
+authRouter.on(["POST", "GET"], "/auth/**", (c) => {
+  return auth.handler(c.req.raw);
+});
+
+const routes = [authRouter] as const;
+
+routes.forEach((route) => {
+  app.basePath("/api").route("/", route);
+});
+
+export type AppType = (typeof routes)[number];
 
 // get auth links
 // get token
