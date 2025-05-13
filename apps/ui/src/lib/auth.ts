030251a7772d48ad986d388caad4794d1620ae8c
Bruno Germano <bruno.germano@gmail.com>
2025-05-13 19:36:43 -0300

    add protected route

--- apps/ui/src/lib/auth.ts
+++ apps/ui/src/lib/auth.ts
@@ -1,7 +1,16 @@
+import { jwt } from "better-auth/plugins";
 import { createAuthClient } from "better-auth/react";
+
+export let token: string | null = null;
 
 export const authClient = createAuthClient({
   baseURL: "http://localhost:3333/", // The base URL of your auth server
+  plugins: [jwt()],
 });
 
-export const { useSession } = authClient;
+export const { useSession, signIn, signUp, signOut } = authClient;
+
+export const retriveToken = async () => {
+  const a = authClient.getSession();
+  return jwt;
+};
