eb699d962c0d48f84690f966e246ba3e44623b7a
Bruno Germano <bruno.germano@gmail.com>
2025-05-26 18:23:19 -0300

    update env vars and constants

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -12,3 +12,7 @@ export const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET as string
 
 export const DATABASE_URL = process.env.DATABASE_URL as string;
 export const DATABASE_AUTH_TOKEN = process.env.DATABASE_AUTH_TOKEN as string;
+
+export const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN as string;
+export const DISCORD_GUILD_ID = process.env.DISCORD_GUILD_ID as string;
+export const DISCORD_ROLE_NAME = process.env.DISCORD_ROLE_NAME as string;
