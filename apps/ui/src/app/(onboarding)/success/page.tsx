8cdfc301a1d0b94105c156f4ce1d52c5ac3b11d8
Bruno Germano <bruno.germano@gmail.com>
2025-05-26 18:25:26 -0300

    protect success route

--- apps/ui/src/app/(onboarding)/success/page.tsx
+++ apps/ui/src/app/(onboarding)/success/page.tsx
@@ -1,21 +1,24 @@
 import { SuccessConfetti } from "@/components/success-confetti";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import ProtectedRoute from "@/providers/protected-route";
 
 export default function SuccessPage() {
   return (
     <div className="flex grow items-center justify-center">
-      <SuccessConfetti/>
-      <Card className="w-full max-w-md shadow-lg">
-        <CardHeader>
-          <CardTitle>Success!</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-center">
-            Thank you for completing the process. Your are done here you role in
-            Discord was updated and you access granted!
-          </p>
-        </CardContent>
-      </Card>
+      <ProtectedRoute>
+        <SuccessConfetti />
+        <Card className="w-full max-w-md shadow-lg">
+          <CardHeader>
+            <CardTitle>Success!</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-center">
+              Thank you for completing the process. Your are done here you role
+              in Discord was updated and you access granted!
+            </p>
+          </CardContent>
+        </Card>
+      </ProtectedRoute>
     </div>
   );
 }
