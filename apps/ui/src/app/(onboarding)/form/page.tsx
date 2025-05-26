e3951fdf3898206271e556f199aaab76643ac7f0
Bruno Germano <bruno.germano@gmail.com>
2025-05-26 18:24:13 -0300

    protect route

--- apps/ui/src/app/(onboarding)/form/page.tsx
+++ apps/ui/src/app/(onboarding)/form/page.tsx
@@ -1,11 +1,14 @@
 import LeadForm from "@/components/lead-form";
+import ProtectedRoute from "@/providers/protected-route";
 
 export default function FormPage() {
   return (
     <div className="flex flex-col gap-6 w-full md:w-[45rem] mx-auto mt-10">
-      <p className="text-sm text-muted-foreground">step 2</p>
-      <h2 className="text-xl font-bold">Tell us about yourself</h2>
-      <LeadForm />
+      <ProtectedRoute>
+        <p className="text-sm text-muted-foreground">step 3</p>
+        <h2 className="text-xl font-bold">Tell us about yourself</h2>
+        <LeadForm />
+      </ProtectedRoute>
     </div>
   );
 }
