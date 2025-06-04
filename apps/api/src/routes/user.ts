11891d0d512ad78efa877cae7075f175d26bc63e
Bruno Germano <bruno.germano@gmail.com>
2025-06-04 19:12:26 -0300

    update user routes

--- apps/api/src/routes/user.ts
+++ apps/api/src/routes/user.ts
@@ -6,6 +6,102 @@ import { Context } from "hono";
 
 const userRoute = createRouter();
 
+userRoute.get(
+  "/user/check",
+  async (
+    c: Context<
+      {
+        Variables: {
+          user: User | null;
+          session: Session | null;
+        };
+      },
+      "*",
+      {}
+    >
+  ) => {
+    const user = c.var.user;
+
+    if (!user) {
+      return c.json(
+        {
+          error: "Unauthorized",
+          message: "You must be logged in to check your user information.",
+        },
+        { status: 401 }
+      );
+    }
+
+    // check user dicord account
+    const accounts = await auth.api.listUserAccounts({
+      headers: c.req.raw.headers,
+    });
+
+    const discordAccount = accounts.find(
+      (account) => account.provider === "discord"
+    );
+
+    if (!discordAccount) {
+      return c.json(
+        {
+          error: "Discord account not linked",
+          message:
+            "You must link your Discord account to check your user information.",
+        },
+        { status: 403 }
+      );
+    }
+
+    // check user is member of the discord server
+    const discordUserId = discordAccount.accountId;
+    let member;
+
+    try {
+      member = await client.getMember(DISCORD_GUILD_ID, discordUserId);
+    } catch (error) {
+      console.error("Error fetching Discord member:", error);
+      return c.json(
+        {
+          error: "Discord member fetch failed",
+          message: (error as Error).message,
+        },
+        { status: 500 }
+      );
+    }
+
+    if (!member) {
+      return c.json(
+        {
+          error: "Discord member not found",
+          message:
+            "You must be a member of the Discord server to check your user information.",
+        },
+        { status: 404 }
+      );
+    }
+
+    // check user dicord role
+    const roleId = await getRoleId(DISCORD_ROLE_NAME, DISCORD_GUILD_ID);
+    const hasRole = member.roles.includes(roleId);
+
+    if (!hasRole) {
+      return c.json(
+        {
+          message:
+            "You have not been assigned the required role in Discord yet.",
+          done: false,
+        },
+        { status: 202 }
+      );
+    }
+
+    return c.json({
+      message: `User is done.`,
+      done: true,
+    });
+  }
+);
+
 userRoute.patch(
   "/user/success",
   async (
@@ -70,7 +166,7 @@ userRoute.patch(
 
       const roleId = await getRoleId(DISCORD_ROLE_NAME, DISCORD_GUILD_ID);
 
-      await client.addRole(DISCORD_GUILD_ID,discordUserId, roleId);
+      await client.addRole(DISCORD_GUILD_ID, discordUserId, roleId);
 
       return c.json({
         message: `User ${userId} updated successfully and Discord role assigned.`,
